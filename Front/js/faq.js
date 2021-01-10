7529a4a3535b1ec4d9b12978aff7eb80e7a92ae7
sduhf <you@exampe.com>
2021-01-10 11:57:08 +0530

    Commited

---
+++ Front/js/faq.js
@@ -0,0 +1,89 @@
+/* =========================================================================
+                                faq.html
+============================================================================ */
+
+// 1
+$("#faq .faq1 .faq-plus")[0].addEventListener("click", function() {
+    $("#faq .faq1 .faq-plus")[0].style.display = "none";
+    $("#faq .faq1 .faq-minus")[0].style.display = "block";
+  });
+  
+  $("#faq .faq1 .faq-minus")[0].addEventListener("click", function() {
+    $("#faq .faq1 .faq-minus")[0].style.display = "none";
+    $("#faq .faq1 .faq-plus")[0].style.display = "block";
+  });
+  
+  // 2
+  
+  $("#faq .faq2 .faq-plus")[0].addEventListener("click", function() {
+    $("#faq .faq2 .faq-plus")[0].style.display = "none";
+    $("#faq .faq2 .faq-minus")[0].style.display = "block";
+  });
+  
+  $("#faq .faq2 .faq-minus")[0].addEventListener("click", function() {
+    $("#faq .faq2 .faq-minus")[0].style.display = "none";
+    $("#faq .faq2 .faq-plus")[0].style.display = "block";
+  });
+  
+  // 3
+  
+  $("#faq .faq3 .faq-plus")[0].addEventListener("click", function() {
+    $("#faq .faq3 .faq-plus")[0].style.display = "none";
+    $("#faq .faq3 .faq-minus")[0].style.display = "block";
+  });
+  
+  $("#faq .faq3 .faq-minus")[0].addEventListener("click", function() {
+    $("#faq .faq3 .faq-minus")[0].style.display = "none";
+    $("#faq .faq3 .faq-plus")[0].style.display = "block";
+  });
+  
+  // 4
+  
+  $("#faq .faq4 .faq-plus")[0].addEventListener("click", function() {
+    $("#faq .faq4 .faq-plus")[0].style.display = "none";
+    $("#faq .faq4 .faq-minus")[0].style.display = "block";
+  });
+  
+  $("#faq .faq4 .faq-minus")[0].addEventListener("click", function() {
+    $("#faq .faq4 .faq-minus")[0].style.display = "none";
+    $("#faq .faq4 .faq-plus")[0].style.display = "block";
+  });
+  
+  // 5
+  
+  $("#faq .faq5 .faq-plus")[0].addEventListener("click", function() {
+    $("#faq .faq5 .faq-plus")[0].style.display = "none";
+    $("#faq .faq5 .faq-minus")[0].style.display = "block";
+  });
+  
+  $("#faq .faq5 .faq-minus")[0].addEventListener("click", function() {
+    $("#faq .faq5 .faq-minus")[0].style.display = "none";
+    $("#faq .faq5 .faq-plus")[0].style.display = "block";
+  });
+  
+  // 6
+  
+  $("#faq .faq6 .faq-plus")[0].addEventListener("click", function() {
+    $("#faq .faq6 .faq-plus")[0].style.display = "none";
+    $("#faq .faq6 .faq-minus")[0].style.display = "block";
+  });
+  
+  $("#faq .faq6 .faq-minus")[0].addEventListener("click", function() {
+    $("#faq .faq6 .faq-minus")[0].style.display = "none";
+    $("#faq .faq6 .faq-plus")[0].style.display = "block";
+  });
+  
+  // 7
+  
+  $("#faq .faq7 .faq-plus")[0].addEventListener("click", function() {
+    $("#faq .faq7 .faq-plus")[0].style.display = "none";
+    $("#faq .faq7 .faq-minus")[0].style.display = "block";
+  });
+  
+  $("#faq .faq7 .faq-minus")[0].addEventListener("click", function() {
+    $("#faq .faq7 .faq-minus")[0].style.display = "none";
+    $("#faq .faq7 .faq-plus")[0].style.display = "block";
+  });
+  
+  
+  
